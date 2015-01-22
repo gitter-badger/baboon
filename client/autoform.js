6f3ea35070eec2f3aa07de9c4257e3ca6730ce48
Gustav Simonsson <gustav.simonsson@gmail.com>
2015-01-22 05:17:49 +0100

    Add confirm box for legal disclaimer

--- client/autoform.js
+++ client/autoform.js
@@ -1,5 +1,16 @@
 AutoForm.hooks({
   submitVulnerabilityForm: {
+    before: {
+      submitVulnerability: function(doc, template) {
+        // TODO: add some nice styling to this popup
+        var r = confirm("TODO: legal disclaimer text here!");
+        if (r == true) {
+          return doc
+        } else {
+          return false
+        }
+      }
+    },
     after: {
       submitVulnerability: function(err, res){
         if(err){
