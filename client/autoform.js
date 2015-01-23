c2b2796d1056b4bd4339e635ab632f12f37174d5
kaeptnjoda <jutta@ethdev.com>
2015-01-23 14:01:06 -0800

    Update autoform.js

--- client/autoform.js
+++ client/autoform.js
@@ -3,7 +3,7 @@ AutoForm.hooks({
     before: {
       submitVulnerability: function(doc, template) {
         // TODO: add some nice styling to this popup
-        var r = confirm("TODO: legal disclaimer text here!");
+        var r = confirm("Please confirm: You have read and understood the rules of the bug bounty program. You are responsible for any tax implications of any reward you receive and must comply with all tax laws applicable to any rewards that we may issue you. We cannot issue rewards to individuals who are on sanctions lists, or who are located in countries that are on sanctions lists. You must comply with all applicable local, state, national, and international laws, rules, and regulations in connection with your participation in this program.");
         if (r == true) {
           return doc
         } else {
